0e978c51798a23f6fcc5c48bac83d06c0348a1fe
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2022-06-19 13:13:24 +1000

    add carloresults object

--- src/py_carlo/sim.py
+++ src/py_carlo/sim.py
@@ -6,7 +6,7 @@ import pandas as pd
 from dataclasses import dataclass
 
 
-from typing import Callable, Optional, Tuple, List, Any
+from typing import Callable, Optional, List, Any
 
 
 @dataclass
@@ -39,6 +39,23 @@ class RunResult:
         return df[['run_id', 'sim_id', 'result']]
 
 
+@dataclass
+class CarloResult:
+    """Results from a set of runs - completed 
+    monte carlo simulations"""
+    num_runs: int
+    num_sims: int
+    results: List[RunResult]
+
+    def as_df(self) -> pd.DataFrame:
+
+        df = pd.concat([
+            result.as_df() for result in self.results
+        ])
+
+        return df
+
+
 class NoResultsError(Exception):
     def __init__(self) -> None:
         super().__init__("No results, please `.run()` first")
@@ -49,7 +66,7 @@ class Simulation:
     seed: int
     num_sims: int
     num_runs: int
-    results: Optional[List[RunResult]]
+    results: Optional[CarloResult]
 
     def __init__(self,
                  sim_function: Callable,
@@ -65,26 +82,22 @@ class Simulation:
         self.sim_function = sim_function
         self.num_sims = num_sims
         self.num_runs = num_runs
-        self.results = None
 
     def _run_sims(self):
         return [SimResult(sim_id, self.sim_function()) for sim_id in range(self.num_sims)]
 
     def run(self) -> None:
-        self.results = [RunResult(run_id, self._run_sims())
-                        for run_id in range(self.num_runs)]
+        self.results = CarloResult(
+            num_sims=self.num_sims,
+            num_runs=self.num_runs,
+            results=[RunResult(run_id, self._run_sims())
+                     for run_id in range(self.num_runs)]
+        )
 
-    def results_to_df(self) -> pd.DataFrame:
+    def result_stats(self) -> pd.DataFrame:
         if self.results is None:
             raise NoResultsError
 
-        df = pd.concat([
-            result.as_df() for result in self.results
-        ])
-
-        return df
-
-    def result_stats(self) -> pd.DataFrame:
-        results = self.results_to_df()
-        stats = results.groupby('run_id')['result'].value_counts()
+        results_df = self.results.as_df()
+        stats = results_df.groupby('run_id')['result'].value_counts()
         return stats
